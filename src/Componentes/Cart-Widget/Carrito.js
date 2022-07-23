0e54c01d2cc0f8c57d444020d6671e5c06e2c0a9
fer1ado <fer1ado@yahoo.com.ar>
2022-07-23 19:58:17 -0300

    Desafio Clase 2 V1.1

---
+++ src/Componentes/Cart-Widget/Carrito.js
@@ -0,0 +1,16 @@
+import "./carrito.css"
+
+
+const Carrito = () => {
+
+    return(
+        <div class="cart-btn algo">
+            <span class="nav-icon">
+                <i class="fas fa-cart-plus fa-xl"></i>
+            </span>
+                <div class="cart-items">0</div>
+        </div>
+    )
+}
+
+export default Carrito
--- src/Componentes/Navbar/navbar.js
+++ src/Componentes/Navbar/navbar.js
@@ -1,5 +1,6 @@
 import "./navbar.css"
 import Button from "../Button/button"
+import Carrito from "../Cart-Widget/Carrito"
 
 const Navbar = () =>{
 
@@ -16,13 +17,9 @@ const Navbar = () =>{
                 <Button color="darkviolet"> Tazas </Button>
                 <Button color="darkviolet"> Botellas </Button>
             </div>
+        
+            <Carrito/>
 
-        <div class="cart-btn algo">
-            <span class="nav-icon">
-                <i class="fas fa-cart-plus fa-xl"></i>
-            </span>
-                <div class="cart-items">0</div>
-        </div>
         </navbar>
     )
 
